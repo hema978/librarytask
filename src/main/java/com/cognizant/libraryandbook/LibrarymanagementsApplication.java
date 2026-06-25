88b77e37e8548c77ebd605669bc796840d6d2836
hema978 <hemagowda.ss.123@gmail.com>
2026-06-25 11:11:17 +0530

    first commit

---
+++ src/main/java/com/cognizant/libraryandbook/LibrarymanagementsApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.libraryandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymanagementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymanagementsApplication.class, args);
+	}
+
+}
